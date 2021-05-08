7a627e322f6741a7822a257c85d79dcf768c58cf
jeg99 <a00824494@itesm.com>
2021-05-08 18:30:28 -0500

    socket for server and client

--- server.c
+++ server.c
@@ -6,6 +6,44 @@
 #include <sys/stat.h>
 #include <syslog.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <time.h> 
+
+void server_init() 
+{
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr; 
+
+    char sendBuff[1025];
+    time_t ticks; 
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff)); 
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5000); 
+
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+
+    listen(listenfd, 10); 
+
+    while(1)
+    {
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+
+        ticks = time(NULL);
+        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+        write(connfd, sendBuff, strlen(sendBuff)); 
+
+        close(connfd);
+        sleep(1);
+     }
+}
 
 static void daemonize()
 {
@@ -74,9 +112,10 @@ int main()
         while (1)
         {
             //TODO: Insert daemon code here.
-            syslog (LOG_NOTICE, "First daemon running.");
-            fflush(fp);
-            sleep (2);
+            // syslog (LOG_NOTICE, "First daemon running.");
+            // fflush(fp);
+            // sleep (2);
+            server_init();
         }
     }
     fclose(fp);
