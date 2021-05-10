3209f6e683fca78059edade1bc574f267679a235
Alam Sanchez <asancheztorres1905@gmail.com>
2021-05-09 23:54:05 -0500

    Add join function

--- main.c
+++ main.c
@@ -6,20 +6,18 @@
 #include "table.h"
 
 int main() {
-    char* columns[3] = {"Fecha",
-                        "Hora",
-                        "Nombre"};
+    char* columns[3] = {"Fecha", "Hora", "Nombre"};
 
     db_table* my_table = createTable(columns, 3, 5);
 
     my_table->rows[0][1] = "16:00";
-    //printf("Second : %s\n", my_table->rows[0][1]);
-    //printf("Column : %s\n", my_table->columns[1]);
-    //printf("Shape: (%d, %d)\n", my_table->shape.rows, my_table->shape.cols);
+    // printf("Second : %s\n", my_table->rows[0][1]);
+    // printf("Column : %s\n", my_table->columns[1]);
+    // printf("Shape: (%d, %d)\n", my_table->shape.rows, my_table->shape.cols);
 
     db_table* parsed = parse("table_1.txt");
-    //printf("%s %d\n", parsed->columns[0], parsed->lastRow);
-    //printTable(parsed);
+    // printf("%s %d\n", parsed->columns[0], parsed->lastRow);
+    // printTable(parsed);
 
     char* selection[] = {"c1", "c3", "c4"};
     db_table* selected = Select(parsed, selection, 3);
@@ -28,5 +26,17 @@ int main() {
     db_table* where = Where(selected, "c4", "ew");
     printTable(where);
 
-    //printTable(parsed);
+    // Join
+    db_table* joinA = parse("join_test_a.csv");
+    db_table* joinB = parse("join_text_b.csv");
+
+    printf("JOIN TEST\n");
+    db_table* joinTable = Join(joinA, joinB);
+
+    printf("JOIN A\n");
+    printTable(joinA);
+    printf("JOIN B\n");
+    printTable(joinB);
+
+    printTable(joinTable);
 }
